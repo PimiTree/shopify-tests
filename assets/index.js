264b5c34a7fbe61a02a5a5ba4da3e78a18280f12
PimiTree <voronenkotg@gmail.com>
2025-06-28 15:24:59 +0300

    feat - header announcement section - header section - hero section

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,41 @@
-console.log('I am Shopify!');
+(() => {
+
+  function closeSignUpAnnouncement () {
+     const signUpAnnouncement = document.querySelector('.sign-up-announcement');
+
+     if (signUpAnnouncement !== null) {
+       signUpAnnouncement.querySelector('.x-mark').addEventListener("click", (e) => {
+         signUpAnnouncement.remove();
+       }, {once: true})
+
+     }
+  }
+
+  function numberCounter() {
+     const scoreElement = document.querySelectorAll('.main-hero-section__achievements-item [data-score]');
+
+    scoreElement.forEach((score) => {
+
+      const time = 3000;
+      const step = 20;
+      const maxSteps = time/step | 0;
+      let current = 1;
+      const numberStep = score.dataset.score / maxSteps;
+
+      const interval = setInterval(() => {
+        score.textContent = `${(numberStep * current) | 0}+`;
+
+        current += 1;
+
+         if (current > maxSteps) {
+           clearInterval(interval);
+         }
+       }, step)
+    })
+  }
+
+
+  closeSignUpAnnouncement();
+  numberCounter();
+
+})()
